f3f206899362e28827195100476ef1d079a070c7
Natmin2548 <nni893399@gmail.com>
2026-08-14 23:52:39 +0700

    chore: remove more temporary scripts

--- server/temp2.cjs
+++
@@ -1,7 +0,0 @@
-const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient();
-async function main() {
-  const user = await prisma.user.findUnique({ where: { email: 'nni893399@gmail.com' } });
-  console.log('Role in DB:', user?.role);
-}
-main().finally(() => prisma.$disconnect());
